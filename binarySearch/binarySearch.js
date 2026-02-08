44d60fdcab0705bace916d485e3d31326989fc3c
JustSemen <semyn03@mail.ru>
2026-02-08 15:47:50 +0300

    fix: finally find the best solve

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -35,17 +35,17 @@
 } */
 
 function search(nums, target) {
-  let low = 0, high = nums.length
+  let low = 0, high = nums.length - 1
 
   while(low <= high) {
     let mid = Math.floor((low + high) / 2)
 
-    if(mid === low) return -1
-
     if(nums[mid] === target) return mid
-    else if (target > nums[mid]) low = mid
-    else if (target < nums[mid]) right = mid 
+    else if (target > nums[mid]) low = mid + 1
+    else if (target < nums[mid]) high = mid - 1
   }
+
+  return -1
 }
 
-console.log(search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10));
+console.log(search([1], 11));
